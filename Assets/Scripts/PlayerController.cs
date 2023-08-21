8f7076cd6e71a8e4a331784fa440d2cc35d88fe2
Misha00025 <102412201+Misha00025@users.noreply.github.com>
2023-08-21 13:42:09 +0300

    Добавлена перемещение персонажа

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,29 +5,87 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 1.0f;
-    [SerializeField] private float _walkSpeed = 0.5f;
+    [SerializeField] private float _walkSpeed = 1.0f;
     [SerializeField] private float _runSpeed = 1.5f;
+    [SerializeField] private float _jumpForce = 10;
+    [SerializeField] private float _gravity = -10;
+    [SerializeField] private KeyCode _runKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
 
+    private float _moveSpeed;
+    private bool _isGrounded = false;
     private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _velocity = Vector3.zero;
 
-    private CharacterController _controller;
+    private CharacterController _controller;    
 
-    private void Awake()
+    private void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        GetReferences();
+        InitVariables();
     }
 
     private void Update()
+    {
+        HandleGravity();
+        HandleJumping();
+        HandleRuning();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(moveX, 0 , moveZ);
         moveDirection = moveDirection.normalized;
+        moveDirection = transform.TransformDirection(moveDirection);
 
+        _controller.Move(moveDirection * _moveSpeed * Time.deltaTime);
+    }
 
+    private void HandleRuning()
+    {
+        if (Input.GetKeyDown(_runKey))
+        {
+            _moveSpeed = _runSpeed;
+        }
+        if (Input.GetKeyUp(_runKey))
+        {
+            _moveSpeed = _walkSpeed;
+        }
+    }
 
-        _controller.Move(moveDirection * Time.deltaTime);
+    private void HandleGravity()
+    {  
+        _controller.Move(_velocity * Time.deltaTime);      
+        _isGrounded = _controller.isGrounded;
+        if (_isGrounded)
+        {
+            _velocity.y = _gravity * 0.1f;
+        }        
+        else
+        {
+            _velocity.y += _gravity * Time.deltaTime;
+        }        
+    }
+
+    private void HandleJumping()
+    {
+        if (_isGrounded && Input.GetKeyDown(_jumpKey))
+        {
+            _velocity.y = _jumpForce;
+        }
+    }
+
+    private void GetReferences()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void InitVariables()
+    {
+        _moveSpeed = _walkSpeed;
     }
 }
