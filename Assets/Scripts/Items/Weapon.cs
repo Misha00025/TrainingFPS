6fa40bc26fc29bd5960da7b7449ab011e93a8ec9
MadBritain <alena_school009@mail.ru>
2023-08-28 15:19:42 +0300

    Добавлены иконки, выстрелы

---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Weapon", menuName = "Items/Weapon")]
+
+public class Weapon : Item
+{
+    public GameObject prefab;
+    public GameObject muzzleFlashParticles;
+    public int magazineSize; //количество пулей в магазине
+    public int magazineCount; //количество магазиноов, которое мы можем хранить
+    public float range; //дальность стрельбы
+    public WeaponType weaponType;
+}
+
+public enum WeaponType { Melee, Pistol, AR, ShotGun, Sniper} //различные виды оружия (пока есть только пистолет и нож)
